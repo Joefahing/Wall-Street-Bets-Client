f5d1b3ceb5e652a8d8e01136d97ee5ce0e35e763
joefahing <cao11d@uwindsor.ca>
2020-12-22 22:28:19 -0500

    added postsFromJson function to process and build posts

--- lib/main.dart
+++ lib/main.dart
@@ -5,17 +5,36 @@ import 'package:flutter/material.dart';
 import 'package:charts_flutter/flutter.dart' as charts;
 import 'package:http/http.dart' as http;
 
-Future<String> fetchPosts() async {
-  final res = await http.get('https://wall-street-bet-server.herokuapp.com/');
+import 'package:wsb_dashboard/model/post.dart';
 
-  if (res.statusCode == 200) {
-    String message = '';
-    Map<String, dynamic> json = jsonDecode(res.body);
-    message = json['message'];
-    return message;
+Future<Map<String, dynamic>> fetchPosts() async {
+  final response = await http.get(
+      'https://wall-street-bet-server.herokuapp.com/stats/gain_loss/post?interval=week');
+
+  if (response.statusCode == 200) {
+    final Map<String, dynamic> json = jsonDecode(response.body);
+    return json;
   } else {
-    throw Exception('Failed to fetch from WSB');
+    throw Exception('Failed to fetch from Gain and Loss end point');
   }
+}
+
+Future<List<Post>> getPosts() async {
+  final json = await fetchPosts();
+  final raw_posts = json['data_used'];
+  final posts = postsFromJson(raw_posts: raw_posts);
+
+  return posts;
+}
+
+List<Post> postsFromJson({List<dynamic> raw_posts}) {
+  final List<Post> posts = [];
+
+  for (var post in raw_posts) {
+    final newPost = Post.fromJson(json: post);
+    posts.add(newPost);
+  }
+  return posts;
 }
 
 void main() {
@@ -23,7 +42,6 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -45,29 +63,54 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  Future<String> message;
+  Future<List<Post>> posts;
 
   @override
   void initState() {
     super.initState();
-    message = fetchPosts();
+    posts = getPosts();
+    print('posts is being fetched');
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Wall Street Bets')),
-      body: FutureBuilder<String>(
-        future: message,
+      body: FutureBuilder<List<Post>>(
+        future: posts,
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            return Text(snapshot.data);
+            final firstPost = snapshot.data[0];
+            final secondPost = snapshot.data[1];
+            final thirdPost = snapshot.data[2];
+
+            return ListView(
+              padding: const EdgeInsets.all(8),
+              children: <Widget>[
+                Container(
+                  height: 50,
+                  color: Colors.amber[600],
+                  child: Center(child: Text(firstPost.title)),
+                ),
+                Container(
+                  height: 50,
+                  color: Colors.amber[500],
+                  child: Center(child: Text(secondPost.title)),
+                ),
+                Container(
+                  height: 50,
+                  color: Colors.amber[100],
+                  child: Center(child: Text(thirdPost.title)),
+                ),
+              ],
+            );
           } else if (snapshot.hasError) {
             return Text("${snapshot.error}");
           }
           return CircularProgressIndicator();
         },
       ),
+      //Text('Hello world'),
     );
   }
 }
