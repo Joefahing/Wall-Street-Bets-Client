7f98da3d307814f3990d3fb7d860bf787fac0795
joefahing <cao11d@uwindsor.ca>
2021-03-06 18:13:37 -0500

    Added styling to menu item and Added styling to logo

--- lib/widgets/side_menu.dart
+++ lib/widgets/side_menu.dart
@@ -1,7 +1,7 @@
 ///The side menu will contain
 ///1. background color: ?
 ///2. Everything is centered
-///3. Name Crayons
+///3. Name Crayonse
 ///4. Menu Items
 /// 1. Wall Strret Bet Index
 /// 2. Trending Symbols
@@ -10,81 +10,142 @@
 ///
 
 import 'package:flutter/material.dart';
+import '../widgets/main_menu_button.dart';
+import '../components/theme_data.dart' as theme;
 
-class SideMenu extends StatelessWidget {
-  final horizontalPadding = 10;
-  final verticalPadding = 10;
+class SideMenu extends StatefulWidget {
+  SideMenu({Key key}) : super(key: key);
+
+  @override
+  _SideMenuState createState() => _SideMenuState();
+}
+
+class _SideMenuState extends State<SideMenu> {
+  final horizontalPadding = 10.0;
+  final verticalPadding = 25.0;
+  final iconSize = 40.0;
+  final backgroundColor = Colors.white;
+
+  final logo = 'assets/images/crayon_logo.png';
+  final algoIcon = 'assets/images/algorithmn_menu.svg';
+  final donationIcon = 'assets/images/donation_menu.svg';
+  final indexMenuIcon = 'assets/images/index_menu.svg';
+  final aboutMeIcon = 'assets/images/man_menu.svg';
+  final subscriptionIcon = 'assets/images/subscription_menu.svg';
+  final settingIcon = 'assets/images/settings_menu.svg';
+  final trendingIcon = 'assets/images/trending.svg';
+
+  String clickedButton = 'index';
+
+  onButtonClicked(String label) {
+    setState(() {
+      clickedButton = label;
+    });
+  }
 
   build(BuildContext context) {
     return Container(
+      padding: EdgeInsets.only(
+        top: horizontalPadding,
+      ),
       height: double.infinity,
-      decoration: BoxDecoration(color: Colors.blue[100]),
+      decoration: BoxDecoration(color: backgroundColor),
       child: Column(
         children: [
           //This row will be come the header of the side menu
-          Row(
-            children: [
-              SizedBox(
-                height: 20,
-                width: 20,
-                child: Container(
-                  color: Colors.blue,
+          Padding(
+            padding: EdgeInsets.only(left: verticalPadding),
+            child: Row(
+              children: [
+                Container(
+                  width: iconSize + 10,
+                  height: iconSize + 10,
+                  decoration: BoxDecoration(
+                    color: theme.darkGrey,
+                    borderRadius: BorderRadius.circular(theme.borderRadius),
+                  ),
+                  child: Center(
+                    child: SizedBox(
+                      height: iconSize,
+                      width: iconSize,
+                      child: Image.asset(logo),
+                    ),
+                  ),
                 ),
-              ),
-              Text('Crayons'),
-            ],
+                SizedBox(width: 10),
+                RichText(
+                  text: TextSpan(
+                    style: theme.headline2,
+                    children: [
+                      TextSpan(text: 'CRA', style: TextStyle(color: theme.fireRed)),
+                      TextSpan(text: 'Y', style: TextStyle(color: theme.sunsetYellow)),
+                      TextSpan(text: 'ONS', style: TextStyle(color: theme.leafGreen)),
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
-          SizedBox(height: 50),
+          SizedBox(height: 40),
           Expanded(
             child: ListView(
               shrinkWrap: true,
               children: [
-                MainMenuButton(text: 'Wall Street Bet Index'),
-                Divider(),
-                MainMenuButton(text: 'Trending Symbols'),
-                Divider(),
-                MainMenuButton(text: 'Algorithm Trading'),
-                Divider(),
-                MainMenuButton(text: 'Subscription'),
-                Divider(),
-                MainMenuButton(text: 'Setting'),
+                MainMenuButton(
+                  text: 'Wall Street Bet Index',
+                  label: 'index',
+                  iconName: indexMenuIcon,
+                  color: clickedButton == 'index' ? theme.darkGreen : theme.mediumGrey,
+                  isSelected: clickedButton == 'index',
+                  verticalPaddings: verticalPadding,
+                  onPress: onButtonClicked,
+                ),
+                MainMenuButton(
+                  text: 'Trending Symbols',
+                  label: 'trending',
+                  iconName: trendingIcon,
+                  color: clickedButton == 'trending' ? theme.darkGreen : theme.mediumGrey,
+                  isSelected: clickedButton == 'trending',
+                  verticalPaddings: verticalPadding,
+                  onPress: onButtonClicked,
+                ),
+                MainMenuButton(
+                  text: 'Algorithm Trading',
+                  label: 'algo',
+                  iconName: algoIcon,
+                  color: clickedButton == 'algo' ? theme.darkGreen : theme.mediumGrey,
+                  isSelected: clickedButton == 'algo',
+                  verticalPaddings: verticalPadding,
+                  onPress: onButtonClicked,
+                ),
+                MainMenuButton(
+                  text: 'Subscription',
+                  label: 'subscription',
+                  iconName: subscriptionIcon,
+                  color: clickedButton == 'subscription' ? theme.darkGreen : theme.mediumGrey,
+                  isSelected: clickedButton == 'subscription',
+                  verticalPaddings: verticalPadding,
+                  onPress: onButtonClicked,
+                ),
+                MainMenuButton(
+                  text: 'Settings',
+                  label: 'settings',
+                  iconName: settingIcon,
+                  color: clickedButton == 'settings' ? theme.darkGreen : theme.mediumGrey,
+                  isSelected: clickedButton == 'settings',
+                  verticalPaddings: verticalPadding,
+                  onPress: onButtonClicked,
+                ),
               ],
             ),
           ),
-          MainMenuButton(text: 'Donation')
+          // Container(
+          //   child: MainMenuButton(
+          //     text: 'Donation',
+          //     iconName: donationIcon,
+          //   ),
+          // )
         ],
-      ),
-    );
-  }
-}
-
-class MainMenuButton extends StatelessWidget {
-  final String text;
-  //final icon;
-
-  MainMenuButton({
-    @required this.text,
-  });
-
-  @override
-  build(BuildContext context) {
-    return Center(
-      child: Material(
-        color: Colors.blue[100],
-        child: InkWell(
-          focusColor: Colors.grey[600],
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Icon(Icons.mail, color: Colors.black, size: 30),
-              SizedBox(
-                width: 20,
-              ),
-              Text(text),
-            ],
-          ),
-          onTap: () => {print('Inkwell is tapped')},
-        ),
       ),
     );
   }
